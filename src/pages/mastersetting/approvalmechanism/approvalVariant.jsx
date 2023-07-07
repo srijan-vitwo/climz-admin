04f90cad4439cb8697a60db0261c558a4659c19d
Srijan Das <sdas@vitwo.in>
2023-07-07 18:35:02 +0530

    claimz approval veriant data manupulation fix

--- src/pages/mastersetting/approvalmechanism/approvalVariant.jsx
+++ src/pages/mastersetting/approvalmechanism/approvalVariant.jsx
@@ -7,36 +7,52 @@ import {
 	Input,
 	Select,
 	Text,
+	useToast,
 } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
+import userLogo from '../../../assets/images/user.png';
 
 const ApprovalVariant = ({ approvalState }) => {
 	const token = localStorage.getItem('token');
 	const navigate = useNavigate();
+	const toast = useToast();
+	let counter = approvalState.length - 1;
 	const [loader, setLoader] = useState(false);
-	const [selectBoxes, setSelectBoxes] = useState([1]); // Initial select box
+	const [isLoading, setIsLoading] = useState(false);
+	const [selectBoxes, setSelectBoxes] = useState([1]);
 	const [empList, setEmpList] = useState();
 	const [employeeId, setEmployeeId] = useState();
 	const [userData, setUserData] = useState([]);
+	const [variantName, setVariantName] = useState();
+	const [variants, setVariants] = useState({
+		variants: [],
+	});
+
+	function toastCall() {
+		return toast({
+			title: 'All Approval Stage Added Already',
+			status: 'warning',
+			duration: 3000,
+			isClosable: true,
+		});
+	}
 
 	const addDynamicSelect = (e) => {
-		e.preventDefault();
-		setSelectBoxes((prevSelectBoxes) => [
-			...prevSelectBoxes,
-			prevSelectBoxes.length + 1,
-		]);
-	};
-	const removeDynamicSelect = (index) => {
-		setSelectBoxes((prevSelectBoxes) =>
-			prevSelectBoxes.filter((_, i) => i !== index)
-		);
+		if (selectBoxes.length < counter) {
+			e.preventDefault();
+			setSelectBoxes((prevSelectBoxes) => [
+				...prevSelectBoxes,
+				prevSelectBoxes.length + 1,
+			]);
+		} else {
+			toastCall();
+		}
 	};
 
 	useEffect(() => {
 		const empList = async () => {
 			try {
-				setLoader(true);
 				const response1 = await fetch(
 					`${process.env.REACT_APP_API_URL}/emp-list`,
 					{
@@ -50,7 +66,6 @@ const ApprovalVariant = ({ approvalState }) => {
 				if (response1.ok) {
 					const data1 = await response1.json();
 					setEmpList(data1.data);
-					setLoader(false);
 				} else {
 					navigate('/login');
 				}
@@ -61,11 +76,15 @@ const ApprovalVariant = ({ approvalState }) => {
 		empList();
 	}, []);
 
-	const handelChange = async (e) => {
-		setEmployeeId(e.target.value);
+	const handelChangeVariants = async (e) => {
+		if (e.target.value == employeeId) {
+			console.log('Please select deffered employee!');
+		} else {
+			setEmployeeId(e.target.value);
+		}
 	};
 
-	const userDetails = async (e) => {
+	const userDetails = async (e, index) => {
 		e.preventDefault();
 		try {
 			const response = await fetch(
@@ -80,9 +99,38 @@ const ApprovalVariant = ({ approvalState }) => {
 
 			if (response.ok) {
 				const data = await response.json();
-
 				const updatedUserData = [...userData, data.data];
 				setUserData(updatedUserData);
+				addDynamicSelect(e);
+
+				// approvalState;
+				let tmpGetStatus = approvalState[index].given_status;
+				let tempStatusMaker =
+					approvalState[approvalState.length - 1].given_status;
+
+				let prevVariantData = JSON.parse(JSON.stringify(variants));
+
+				if (prevVariantData.variants[index] == undefined) {
+					let tempVariantUser = {
+						vm_id: '',
+						user_id: employeeId,
+						get_status: tmpGetStatus,
+						status_maker: tempStatusMaker,
+						name: data.data.emp_name,
+						img: data.data.profile_photo,
+					};
+					if (index > 0) {
+						prevVariantData['variants'][index - 1]['status_maker'] =
+							tmpGetStatus;
+					}
+					prevVariantData.variants[index] = tempVariantUser;
+				} else {
+					prevVariantData.variants[index].user_id = employeeId;
+					prevVariantData.variants[index].name = data.data.emp_name;
+					prevVariantData.variants[index].img =
+						data.data.profile_photo;
+				}
+				setVariants(prevVariantData);
 			} else {
 				navigate('/login');
 			}
@@ -91,8 +139,42 @@ const ApprovalVariant = ({ approvalState }) => {
 		}
 	};
 
-	const newArray = [userData[userData.length - 1]];
+	const newArray = [variants?.variants[variants?.variants?.length - 1]];
+	// console.log(userData, 'userData');
 	console.log(newArray, 'newArray');
+	// console.log(selectBoxes, 'selectBoxes');
+	// console.log(approvalState, 'approvalState');
+	console.log(variants, 'variants');
+	const approvalSubmit = async (e) => {
+		e.preventDefault();
+		let formData = new FormData();
+		formData.append('variant_name', variantName);
+		formData.append('variants', JSON.stringify(variants.variants));
+		// formData.append('cost_center', `["${inputs.join('","')}"]`);
+
+		try {
+			setIsLoading(true);
+			const response = await fetch(
+				`${process.env.REACT_APP_API_URL}/claim-approval-save`,
+				{
+					method: 'POST',
+					body: formData,
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			);
+
+			if (response.ok) {
+				toastCall();
+				setIsLoading(false);
+			} else {
+				navigate('/login');
+			}
+		} catch (error) {
+			navigate('/login');
+		}
+	};
 
 	return (
 		<>
@@ -102,12 +184,18 @@ const ApprovalVariant = ({ approvalState }) => {
 					display: 'flex',
 					flexDirection: 'column',
 					alignItems: 'flex-end',
-				}}>
+				}}
+				onSubmit={approvalSubmit}>
 				<Box w='100%'>
 					<Box>
 						<FormControl>
 							<FormLabel>Variant Name</FormLabel>
-							<Input type='text' />
+							<Input
+								type='text'
+								placeholder='Enter Name'
+								required
+								onChange={(e) => setVariantName(e.target.value)}
+							/>
 						</FormControl>
 					</Box>
 					<Box
@@ -127,7 +215,7 @@ const ApprovalVariant = ({ approvalState }) => {
 											width='95%'
 											mr='10px'
 											placeholder='Select Approval'
-											onChange={handelChange}>
+											onChange={handelChangeVariants}>
 											{empList?.map((data, index) => {
 												return (
 													<option
@@ -139,33 +227,8 @@ const ApprovalVariant = ({ approvalState }) => {
 											})}
 										</Select>
 									</FormControl>
-									{index !== 0 && (
-										<Button
-											bgGradient='linear(180deg, #2267A2 0%, #0D4675 100%)'
-											boxShadow='0px 4px 4px rgba(0, 0, 0, 0.25)'
-											color='white'
-											mr='10px'
-											_hover={{
-												bgGradient:
-													'linear(180deg, #2267A2 0%, #0D4675 100%)',
-											}}
-											_active={{
-												bgGradient:
-													'linear(180deg, #2267A2 0%, #0D4675 100%)',
-											}}
-											_focus={{
-												bgGradient:
-													'linear(180deg, #2267A2 0%, #0D4675 100%)',
-											}}
-											p='17px'
-											w='10%'
-											onClick={() =>
-												removeDynamicSelect(index)
-											}>
-											<i className='fa-solid fa-trash'></i>
-										</Button>
-									)}
-									<Button
+
+									{/* <Button
 										bgGradient='linear(180deg, #2267A2 0%, #0D4675 100%)'
 										boxShadow='0px 4px 4px rgba(0, 0, 0, 0.25)'
 										color='white'
@@ -186,7 +249,7 @@ const ApprovalVariant = ({ approvalState }) => {
 										onClick={addDynamicSelect}
 										w='10%'>
 										<i className='fa-sharp fa-solid fa-plus'></i>
-									</Button>
+									</Button> */}
 									<Button
 										bgGradient='linear(180deg, #2267A2 0%, #0D4675 100%)'
 										boxShadow='0px 4px 4px rgba(0, 0, 0, 0.25)'
@@ -205,7 +268,7 @@ const ApprovalVariant = ({ approvalState }) => {
 											bgGradient:
 												'linear(180deg, #2267A2 0%, #0D4675 100%)',
 										}}
-										onClick={userDetails}
+										onClick={(e) => userDetails(e, index)}
 										w='10%'>
 										Apply
 									</Button>
@@ -214,20 +277,22 @@ const ApprovalVariant = ({ approvalState }) => {
 						</Box>
 						<Box width='58%'>
 							<div className='timeline'>
-								{userData?.map((data, index) => {
-									return (
+								{Object.entries(variants?.variants)?.map(
+									([key, value]) => (
 										<>
 											<div
 												className='timeline__event animated fadeInUp timeline__event--type2'
-												key={index}>
+												key={key}>
 												<Box className='timeline__event__icon'>
 													<Image
 														src={
-															data?.profile_photo
+															value?.img == null
+																? userLogo
+																: value?.img
 														}
-														alt={data?.emp_name}
+														alt={value?.name}
 														h='50px'
-														w='100%'
+														w='50'
 														mr='10px'
 													/>
 												</Box>
@@ -241,14 +306,14 @@ const ApprovalVariant = ({ approvalState }) => {
 															fontSize='1.4rem'
 															fontWeight='600'
 															color='var(--chakra-colors-claimzTextBlueLightColor)'>
-															{data?.emp_name}
+															{value?.name}
 														</Text>
 													</Box>
 												</Box>
 											</div>
 										</>
-									);
-								})}
+									)
+								)}
 								{newArray?.map((data, index) => {
 									return (
 										<>
@@ -258,11 +323,13 @@ const ApprovalVariant = ({ approvalState }) => {
 												<Box className='timeline__event__icon'>
 													<Image
 														src={
-															data?.profile_photo
+															data?.img == null
+																? userLogo
+																: data?.img
 														}
-														alt={data?.emp_name}
+														alt={data?.name}
 														h='50px'
-														w='100%'
+														w='50px'
 														mr='10px'
 													/>
 												</Box>
@@ -276,7 +343,7 @@ const ApprovalVariant = ({ approvalState }) => {
 															fontSize='1.4rem'
 															fontWeight='600'
 															color='var(--chakra-colors-claimzTextBlueLightColor)'>
-															{data?.emp_name}
+															{data?.name}
 														</Text>
 													</Box>
 												</Box>
