25b04c843601975b867359b8e277c162e6d4fe1d
Srijan Das <sdas@vitwo.in>
2023-07-11 18:34:46 +0530

    css fix

--- src/pages/mastersetting/tdscalculator/tdsform.jsx
+++ src/pages/mastersetting/tdscalculator/tdsform.jsx
@@ -122,7 +122,18 @@ const Tdsform = ({ rowData }) => {
 						</Box>
 					</DrawerHeader>
 
-					<DrawerBody></DrawerBody>
+					<DrawerBody>
+						<Box
+							background='#F6F9F8'
+							border='1px solid #CECECE'
+							boxShadow='3px 3px 4px rgba(0, 0, 0, 0.25)'
+							borderRadius='6px'
+							padding='0px 10px'
+							display='flex'>
+							<Text>Particulars</Text>
+							<Text>Value</Text>
+						</Box>
+					</DrawerBody>
 				</DrawerContent>
 			</Drawer>
 		</>
